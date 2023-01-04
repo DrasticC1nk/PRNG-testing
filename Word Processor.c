205af2ed01981ae39a0649ca5c8fcac9051dd912
Saksham Mahajan <96070694+DrasticC1nk@users.noreply.github.com>
2023-01-05 01:27:25 +0530

    Add files via upload
    It is supposed to let users write what they want and print it on the screen. I will make it so the final written piece goes into a new file. But for now, this is not working.

---
+++ Word Processor.c
@@ -0,0 +1,64 @@
+//NOT WORKING
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 4096
+
+int main()
+{
+  char **strings;
+
+  int total = 0, ctr;
+  
+  char buffer[BUFFER_SIZE];
+
+  int length = 0;
+
+  printf("\nHit the enter key to start typing:");
+  scanf("");
+  
+  while (getchar() != '\n');
+  
+  printf("\n");
+
+  for (int i = 0; i < 1; i++)
+  {
+    printf(">>> ");
+    fgets(buffer, BUFFER_SIZE, stdin);
+    ctr++;
+
+    if(stdin == "_EXIt_");
+    {
+        break;
+    }
+    
+    length = strlen(buffer);
+
+    buffer[length - 1] = '\0';
+
+    strings[i] = malloc(length * sizeof(char));
+
+    strcpy(strings[i], buffer);
+
+  }
+
+  strings = malloc(ctr * sizeof(char *));
+  
+  printf("\n\n");
+  for (int i = 0; i < ctr; i++)
+  {
+    printf("> %s\n", strings[i]);
+  }
+  printf("\n");
+
+  for(int i=0; i < ctr; i++)
+  {
+    free(strings[i]);
+  }
+
+  free(strings);
+  
+  return 0;
+}
